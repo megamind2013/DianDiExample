ab42d989589bbe231ecd31dcfdd287387e30aafb
Jichao Liu <jichaol@opera.com>
2020-07-23 15:19:45 +0800

    pandas

---
+++ src/main/python/021旋转数据表(行&转换).py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+#coding:utf-8
+import pandas as pd
+import matplotlib.pyplot as plt
+
+pd.options.display.max_columns = 777
+videos = pd.read_excel('Temp/Videos.xlsx', index_col='Month')  # , index_col='Field'
+table = videos.transpose()
+print(table)
